8bddb40ba1a208c928284dfab1610bb4c7e02ce7
kamizox <kamrankhan0368@gmail.com>
2026-05-16 00:07:01 +0500

    Text changed

--- lib/points.dart
+++ lib/points.dart
@@ -340,7 +340,7 @@ class _PointsState extends State<Points> {
                 ),
               ),
               const SizedBox(height: 20.0),
-              Text("Add UPI Id", style: AppWidget.normaltextstyle(20.0)),
+              Text("Add Account No", style: AppWidget.normaltextstyle(20.0)),
               const SizedBox(height: 10.0),
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 10.0),
